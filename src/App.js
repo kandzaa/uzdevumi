7d6769b5c40698bd2b57d17c0e98c6467d406a3f
KandzaP <kandzapure@gmail.com>
2023-10-10 15:19:33 +0300

    yup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const Uzdevumi = 
+  [{
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": false
+  },
+  {
+    "userId": 2,
+    "id": 2,
+    "title": "et porro tempora",
+    "completed": true
+  },
+  {
+    "userId": 6,
+    "id": 3,
+    "title": "fugiat veniam minus",
+    "completed": false
+  },
+  {
+    "userId": 31,
+    "id": 4,
+    "title": "quis ut nam facilis et officia qui",
+    "completed": true
+  },
+  {
+    "userId": 12,
+    "id": 5,
+    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    "completed": false
+  },
+  {
+    "userId": 85,
+    "id": 6,
+    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+    "completed": false
+  }];
+
+
+  function App() {
+    return (
+      <>
+        {Uzdevumi.map((props)=>(
+          <div key={props.id}>
+            <input type="checkbox" checked={props.completed}/>
+            <label>{props.title}</label>
+            
+          </div>
+        ))}; 
+        
+      </>
+    );
+  };
+
 
 export default App;
